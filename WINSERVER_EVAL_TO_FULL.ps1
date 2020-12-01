30a2f25d9f0d9b02a0b4e9d1bcbe16090a465b6f
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2020-12-01 15:01:57 -0300

    Update WINSERVER_EVAL_TO_FULL.ps1

--- WINSERVER_EVAL_TO_FULL.ps1
+++ WINSERVER_EVAL_TO_FULL.ps1
@@ -1,3 +1,7 @@
 DISM /online /Set-Edition:ServerDatacenter /ProductKey:WMDGN-G9PQG-XVVXX-R3X43-63DFG /AcceptEula
 slmgr /ipk YOUR-KEY-HERE
 slmgr /ato
+
+
+##
+dism /online /Set-Edition:ServerStandard /ProductKey:WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY /AcceptEula
