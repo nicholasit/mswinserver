e495e8d31264aa9cdb85a9ad4d5afe70ce09eefb
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2024-04-09 13:47:15 -0300

    Add product-keys for w2k22

--- WINSERVER_EVAL_TO_FULL.ps1
+++ WINSERVER_EVAL_TO_FULL.ps1
@@ -5,3 +5,13 @@ slmgr /ato
 
 ##
 dism /online /Set-Edition:ServerStandard /ProductKey:WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY /AcceptEula
+
+
+
+##### Keys
+
+# Windows Server 2019 Standard N69G4-B89J2–4G8F4-WWYCC-J464C
+# Windows Server 2019 Datacenter WMDGN-G9PQG-XVVXX-R3X43–63DFG
+# Windows Server 2022 Standard VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+# Windows Server 2022 Azure Edition NTBV8–9K7Q8-V27C6-M2BTV-KHMXV
+# Windows Server 2022 Datacenter WX4NM-KYWYW-QJJR4-XV3QB-6VM33
